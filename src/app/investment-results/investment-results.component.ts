07dc51ff641b4801b23ac68ffe9fd2537e7fdf76
Przemyslaw Jambor <przemyslaw.jambor@gmail.com>
2025-11-30 21:27:10 +0100

    Code refactoring

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { inject } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { InvestmentResultsService } from './investment-results.service';
